5433444f01bf395e064d3b56664129862ad62469
ArthurLopes03 <116575676+ArthurLopes03@users.noreply.github.com>
2024-09-27 17:41:25 +0100

    Push 3

--- Week 2 Project/tower.gd
+++ Week 2 Project/tower.gd
@@ -5,14 +5,21 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	for stackHeight in range(height):
+		var stack = stack_Scene.instantiate()
+		var rng = RandomNumberGenerator.new()
+		var pos = Vector3(rng.randf_range(-0.3, 0.3), stackHeight, 0)
+		
+		var mat = StandardMaterial3D.new()
+		
+		mat.albedo_color = Color(100, 100, 100)
+		
+		stack.Node3D.MeshInstance3D.material = mat
+		stack.position = pos
+		add_child(stack)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	for stackHeight in range(height):
-		var stack = stack_Scene.instantiate()
-		var pos = Vector3(0, stackHeight, 0)
-		stack.position = pos
-		add_child(stack)
 	pass
