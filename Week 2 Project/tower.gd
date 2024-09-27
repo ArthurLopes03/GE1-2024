80ed9a5f479535f48c678a00fdb2b2a72433f4e2
ArthurLopes03 <116575676+ArthurLopes03@users.noreply.github.com>
2024-09-27 17:06:49 +0100

    Push 2

--- Week 2 Project/tower.gd
+++ Week 2 Project/tower.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export var height = 5
+@export var stack_Scene:PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	for stackHeight in range(height):
+		var stack = stack_Scene.instantiate()
+		var pos = Vector3(0, stackHeight, 0)
+		stack.position = pos
+		add_child(stack)
 	pass
